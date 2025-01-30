c21cf2a9c6ef7792f182b94103205f34499a1d21
Amnher <amnher@gmail.com>
2025-01-30 20:07:39 +0800

    Add user fetching functionality and update UI to display users

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_zkteco/flutter_zkteco.dart';
 import 'package:flutter_zkteco/src/model/attendance_log.dart';
+import 'package:flutter_zkteco/src/model/user_info.dart';
+import 'package:flutter_zkteco/src/user.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,16 +27,18 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   ZKTeco? fingerprintMachine;
   List<AttendanceLog> logs = [];
+  List<UserInfo> users = [];
 
   @override
   void initState() {
     super.initState();
-    fingerprintMachine = ZKTeco('192.168.110.201', port: 4370);
+    fingerprintMachine = ZKTeco('192.168.110.202', port: 4370);
   }
 
   void clearLogs() {
     setState(() {
       logs = [];
+      users = [];
     });
   }
 
@@ -48,6 +52,22 @@ class _MyHomePageState extends State<MyHomePage> {
           await fingerprintMachine!.getAttendanceLogs();
       setState(() {
         logs = fetchedLogs;
+      });
+      await fingerprintMachine!.disconnect();
+    } else {
+      print('Failed to connect.');
+    }
+  }
+
+  Future<void> connectAndFetchUsers() async {
+    await fingerprintMachine!.initSocket();
+    print('Socket initialized.');
+    bool isConnected = await fingerprintMachine!.connect();
+
+    if (isConnected) {
+      List<UserInfo> fetchedUsers = await fingerprintMachine!.getUsers();
+      setState(() {
+        users = fetchedUsers;
       });
       await fingerprintMachine!.disconnect();
     } else {
@@ -75,22 +95,36 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Text('Fetch Logs'),
                   ),
                   ElevatedButton(
+                    onPressed: connectAndFetchUsers,
+                    child: Text('Fetch Users'),
+                  ),
+                  ElevatedButton(
                     onPressed: clearLogs,
-                    child: Text('Clear Logs'),
+                    child: Text('Clear Logs and Users'),
                   ),
                 ],
               ),
             ),
             Expanded(
-              child: ListView.builder(
-                itemCount: logs.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text('User ID: ${logs[index].id}'),
-                    subtitle: Text('Timestamp: ${logs[index].timestamp}'),
-                  );
-                },
-              ),
+              child: users.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: users.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text('User ID: ${users[index].userId}'),
+                          subtitle: Text('Name: ${users[index].name}'),
+                        );
+                      },
+                    )
+                  : ListView.builder(
+                      itemCount: logs.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text('User ID: ${logs[index].id}'),
+                          subtitle: Text('Timestamp: ${logs[index].timestamp}'),
+                        );
+                      },
+                    ),
             ),
           ],
         ),
